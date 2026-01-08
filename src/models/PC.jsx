ea0cfda983b553e79c20b88d85276b95fed0279c
michelle-b7 <bai7michelle@gmail.com>
2026-01-08 13:09:27 -0800

    added more stickers

--- src/models/PC.jsx
+++ src/models/PC.jsx
@@ -1,5 +1,5 @@
 import React, {useRef, useState} from 'react'
-import {useGLTF, useTexture, Decal} from '@react-three/drei'
+import {useGLTF, useTexture, Decal, Html} from '@react-three/drei'
 import {a} from '@react-spring/three' 
 import PCscene from '../assets/pc.glb'
 import myCustomShell from '../assets/edited_pc_mesh.png'
@@ -9,6 +9,8 @@ import instaPixel from '../assets/insta_pixel.png'
 import githubPixel from '../assets/github_pixel.png'
 import linkedinPixel from '../assets/linked_in_pixel.png'
 import resumePixel from '../assets/resume_pixel.png'
+import uclaPixel from '../assets/ucla_pixel.png'
+import emailPixel from '../assets/email_pixel.png'
 
 export function PC(props) {
   const pcRef = useRef()
@@ -18,7 +20,9 @@ export function PC(props) {
   const { nodes, materials } = useGLTF(PCscene)
   const texture = useTexture(myCustomShell)
 
-  const [instaTex, githubTex, linkedinTex, resumeTex] = useTexture([instaPixel, githubPixel, linkedinPixel, resumePixel])
+  const [instaTex, githubTex, linkedinTex, resumeTex, uclaTex, emailTex] = useTexture([instaPixel, githubPixel, linkedinPixel, resumePixel, uclaPixel, emailPixel])
+  const [copied, setCopied] = React.useState(false);
+
 
   const handleHover = (name, isHovering) => {
     setHoveredLogo(isHovering ? name : null)
@@ -45,7 +49,7 @@ export function PC(props) {
                     {/* insta */}
                     <Decal 
                         position={[-0.9, 1.6, -1.2]} 
-                        rotation={[0, -Math.PI / 2, 0]} 
+                        rotation={[.03, -Math.PI / 2, 0]} 
                         scale={[0.4, 0.4, 0.4]}
                         onClick={() => window.open('https://instagram.com/yun._.michelle', '_blank')}
                         onPointerOver={() => handleHover('insta', true)}
@@ -62,10 +66,27 @@ export function PC(props) {
                         />
                     </Decal>
 
+                    {/* ucla */}
+                    <Decal 
+                        position={[-0.9, 1.15, -0.7]} 
+                        rotation={[-.1, -Math.PI / 2, 0]} 
+                        scale={[0.5, 0.3, 0.4]}
+                    >
+
+                        <meshStandardMaterial 
+                            map={uclaTex} 
+                            transparent
+                            polygonOffset
+                            polygonOffsetFactor={-10}
+                            emissive={hoveredLogo === 'ucla' ? '#ffffff' : '#000000'}
+                            emissiveIntensity={0}
+                        />
+                    </Decal>
+
                     {/* github */}
                     <Decal 
-                    position={[1, 1.62, -.85]} // Positive X (0.9) to put it on the opposite side
-                    rotation={[0, Math.PI / 2, 0]} // Flipped rotation (PI/2) to face the right wall
+                    position={[1, 1.62, -.85]} 
+                    rotation={[0, Math.PI / 2, 0]} 
                     scale={[0.3, 0.3, 0.3]} 
                     onClick={() => window.open('https://github.com/michelle-b7', '_blank')}
                     onPointerOver={() => handleHover('github', true)}
@@ -81,10 +102,47 @@ export function PC(props) {
                     />
                     </Decal>
 
+                    {/* email */}
+                    <Decal 
+                    position={[1.0, 1.85, -1.4]} 
+                    rotation={[-.1, Math.PI / 2, 0]} 
+                    scale={[0.4, 0.4, 0.3]} 
+                    onClick={() => {navigator.clipboard.writeText('mibai@g.ucla.edu'); setCopied(true); setTimeout( () => setCopied(false), 10000);}}
+                    onPointerOver={() => handleHover('email', true)}
+                    onPointerOut={() => handleHover('email', false)}
+                    >
+                    <meshStandardMaterial 
+                        map={emailTex} 
+                        transparent
+                        polygonOffset
+                        polygonOffsetFactor={-10}
+                        emissive={hoveredLogo === 'email' ? '#ffffff' : '#000000'}
+                        emissiveIntensity={1.5}
+                    />
+                    {copied && (
+                      <Html position={[1.0, 2.1, -1.4]} center>
+                        <div style={{
+                          background: 'pink',
+                          color: 'white',
+                          padding: '4px 10px',
+                          borderRadius: '4px',
+                          fontSize: '16px',
+                          whiteSpace: 'nowrap',
+                          pointerEvents: 'none',
+                          fontFamily: 'sans-serif'
+                        }}>
+                          Email copied!
+                        </div>
+                      </Html>
+                    )}
+
+
+                    </Decal>
+
                     {/* linked in */}
                     <Decal 
                     position={[0.9, 1.08, -1.23]} // Positive X (0.9) to put it on the opposite side
-                    rotation={[0, Math.PI / 2, 0]} // Flipped rotation (PI/2) to face the right wall
+                    rotation={[-.04, Math.PI / 2, 0]} // Flipped rotation (PI/2) to face the right wall
                     scale={[0.5, 0.2, 0.3]} 
                     onClick={() => window.open('https://www.linkedin.com/in/michelle-y-bai', '_blank')}
                     onPointerOver={() => handleHover('linkedin', true)}
