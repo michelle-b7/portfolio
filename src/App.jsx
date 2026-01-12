7e3ce4a79c346228f2e46d716e60d18bfc27e033
michelle-b7 <bai7michelle@gmail.com>
2026-01-11 23:59:04 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
         <Navbar/>
         
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="" element={<Home />} />
               <Route path="projects" element={<Projects />} />
             </Routes>
           
